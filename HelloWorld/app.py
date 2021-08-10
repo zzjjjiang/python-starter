cd956e4e19512daa4f74088739f204b7ab5d5576
Martin Burolla <mburolla@gmail.com>
2021-08-10 13:44:24 -0500

    update

--- HelloWorld/app.py
+++ HelloWorld/app.py
@@ -5,9 +5,18 @@
 # Desc: Python 101
 #
 
-r = 50 % 2
-#r = (3 * 2) / 2
-print(r)
+#r = 22 % 10 
+#r = 3 * 2 + 2
+#print(r)
 
+
+# Equality vs Identity
+l1 = ['1', '2', '3']
+l2 = ['1', '2', '3']
+l3 = l1
+print(l1 is l2) # False Identity
+print(l1 is l3) # True Identity
+print(l1 == l2) # True Equality, these are different references but they have an equal value.
+print(l1 == l3) # True, same reference to one list
 
 
