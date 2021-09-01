2e8f46089a7ce2c87ae182bd5c9355dbe40a2a51
Martin Burolla <mburolla@gmail.com>
2021-09-01 10:00:40 -0500

    update

--- Collections/lcrg.py
+++ Collections/lcrg.py
@@ -7,8 +7,10 @@
 # Format: 
 #  [{REQUIRED: expression for each item} {REQUIRED: iteration} {OPTIONAL: if condition}]
 #
-# PROTIP: If you are filtering and mapping on a list, use a list comprehension 
-#         instead of using the filter() and map() functions.
+# PROTIPs: 
+#   If you are filtering and mapping on a list, use a list comprehension 
+#   instead of using the filter() and map() functions.
+#       
 
 people = [
   { "firstname": "joe", "lastname" : "smith", "age": 30 },
@@ -17,8 +19,14 @@ people = [
   { "firstname": "sue", "lastname" : "anderson", "age" : 51 },
 ]
 
+def formatName(p):
+  return f"{p['firstname'].upper()} {p['lastname'].upper()}"
+
 l1 = [p['firstname'] for p in people]
 print(l1) # ['joe', 'peter', 'mary', 'sue']
 
 l2 = [p['firstname'] for p in people if p['age'] > 40]
 print(l2) # ['mary', 'sue']
+
+l3 = [formatName(p) for p in people if p['age'] > 40]
+print(l3) # ['MARY JANE', 'SUE ANDERSON']
