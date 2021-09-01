fd92008d5be32462c05faed235b27014c012aeb3
Martin Burolla <mburolla@gmail.com>
2021-09-01 12:51:24 -0500

    update

--- Collections/list-comp.py
+++ Collections/list-comp.py
@@ -4,21 +4,22 @@
 # Date: 8/17/2021
 # Desc: List comprehensions
 #       Works like a map() but more clear to the reader.
-#       Format: [{expression to execute on each item} {iteration} {if condition}]
-#               [map()  items  filter()]
+#       Format: 
+#         [{Required: expression to execute on each item} {Requried: for iteration} {Optional: if condition}]
+#         [Required: map()  Required: for items  Optional: filter()]
 #
 
 # numbers = [1, 2, 3, 4, 5, 6]
 # doubledList = [(number * 2) for number in numbers if (number % 2 == 0)]  
 # print(doubledList)
 
-people = [
+peopleList = [
   { "firstname": "joe", "lastname" : "smith", "age": 30 },
   { "firstname": "peter", "lastname" : "jones", "age": 40 },
   { "firstname": "mary", "lastname" : "jane", "age" : 50 },
   { "firstname": "sue", "lastname" : "anderson", "age" : 51 },
 ]
-lessPeople = [ person['firstname'] for person in people if person['age'] > 40]
+lessPeople = [ person['firstname'] for person in peopleList if person['age'] > 40 ]
 print(lessPeople)
 
 # people = [
