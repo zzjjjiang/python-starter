f2b90e6df9ec4baf2f7ae2201d81d4256053b5ab
Martin Burolla <mburolla@gmail.com>
2021-09-02 09:15:11 -0500

    update

--- Decision/tfscrg.py
+++ Decision/tfscrg.py
@@ -12,20 +12,26 @@
 #   ()    
 #   []     
 #   {}
+# Everything else is truthy
+#
 
 # https://www.freecodecamp.org/news/truthy-and-falsy-values-in-python/
 
 b1 = True
 b2 = False
-n1 = 54
-n2 = None
-n3 = 0
-l1 = ['red', 'green', 'blue']
-l2 = []
-s1 = ""
-s2 = "test"
+n1 = 54 # truthy
+n2 = None # falsy
+n3 = 0  # falsy
+n4 = -1 # truthy
+n5 = 1  # truthy
+l1 = ['red', 'green', 'blue'] # truthy
+l2 = [] # falsy
+s1 = "" # falsy
+s2 = "test" # truthy
 
-if b1:
+if b1 or n3 or b1:
   print('TRUE')
 else:
   print('FALSE')
+
+ 
