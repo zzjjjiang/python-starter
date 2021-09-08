117f44c80a873aa2b1ff769ef0e8dcd9a3e2530e
Martin Burolla <mburolla@gmail.com>
2021-09-08 15:00:02 -0500

    update

--- DataAccess/app.py
+++ DataAccess/app.py
@@ -9,18 +9,14 @@ import boto3
 import pymysql
 from AuroraDBProxy import AuroraDBProxy
 
-def main():
-  # with AuroraDBProxy() as auroraDBProxy:
-  #   data = auroraDBProxy.getAllPeople()
-  # print(data)
 
+def getPeople():
+  retval = []
   USER = 'sia-db-user'
   HOST = 'sia-db-cluster-instance-1.cosgu9wr5iwp.us-east-1.rds.amazonaws.com'
   PWD = 'testtest'
   DB = 'sia-db'
 
-  client = boto3.client('rds')
-  
   connection = pymysql.connect(
     auth_plugin_map = {'mysql_cleartext_password': None},
     user = USER,
@@ -31,9 +27,12 @@ def main():
   with connection.cursor(pymysql.cursors.DictCursor) as cursor:
     cursor.execute('select * from person')
   connection.commit()
-  people = cursor.fetchall()
+  retval = cursor.fetchall()
+  return retval
+
+def main():
+  people = getPeople()
   print(people)
 
-  
 if __name__ == "__main__":  
   main()
