65f56f4e8de9899936f07ae70437fb27f2ade6b1
Martin Burolla <mburolla@gmail.com>
2021-09-09 11:06:48 -0500

    update

--- DataAccess/data-access.py
+++ DataAccess/data-access.py
@@ -7,6 +7,23 @@
 
 import pymysql
 
+def createConnection():
+  retval = None  
+    
+  # Connecting...
+  USER = 'sia-db-user'
+  HOST = 'sia-db-cluster-instance-1.cosgu9wr5iwp.us-east-1.rds.amazonaws.com'
+  PWD = 'testtest'
+  DB = 'sia-db'
+
+  retval = pymysql.connect(
+    user = USER,
+    host = HOST,
+    password = PWD,
+    database = DB)
+
+  return retval
+
 def getPeople():
   retval = []
 
@@ -29,8 +46,18 @@ def getPeople():
   retval = cursor.fetchall()
   return retval
 
+def insertPerson(personName):
+  retval = 0
+  conn = createConnection()
+  with conn.cursor() as cursor:
+    cursor.execute("insert into person (name) values (%s)", (personName))
+    retval = cursor.lastrowid
+  conn.commit()
+  return retval
 
 def main():
+  id = insertPerson('Tom')
+  print(f"Person Id: {id}.")
   people = getPeople()
   print(people)
 
