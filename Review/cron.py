8babcecd43060b1eca4abdff67626cdb34ca29fa
Martin Burolla <mburolla@gmail.com>
2021-10-04 09:51:53 -0400

    u

---
+++ Review/cron.py
@@ -0,0 +1,26 @@
+#
+# File: cron.py
+# Auth: Martin Burolla
+# Date: 10/4/2021
+# Desc: Task template
+#       Runs forever until Control+C is entered on the console.
+#
+
+from time import sleep
+from datetime import datetime
+
+INTERVAL_SECONDS = 2 # The number of seconds to sleep.
+
+def businessLogic():
+  # Do some real work here...
+  pass
+
+def main():
+  while True:
+    businessLogic()
+    timestamp = datetime.today().strftime('%m/%d/%Y %I:%M:%S %p')
+    print(f'Last run: {timestamp}')
+    sleep(INTERVAL_SECONDS)
+
+if __name__ == "__main__":  
+  main()
