e1266c0876755be8ed833e471fe49d904bcbf1d0
Martin Burolla <mburolla@gmail.com>
2021-10-07 13:33:27 -0400

    u

--- Review/Example Code/s3-data-access.py
+++ Review/Example Code/s3-data-access.py
@@ -5,26 +5,33 @@
 # Desc: Wraps S3
 #
 
-import boto3
+import boto3 # <== Need this to upload to S3.
 
 from time import sleep
 from datetime import datetime
 
-def main():
+def createFileS3(customerList, fileHeader, bucketName, keyPath):
+  retval = None
   client = boto3.client('s3')
-
-  # Our customer IDs.
-  billableCustomersList = ['123', '456', '789']
   
   # Create file contents.
   timeString = datetime.now().strftime("%m/%d/%Y %I:%M:%S %p")
-  fileContents = f'*** BILLABLE CUSTOMERS ***\nTime: {timeString}\n'
-  for customerId in billableCustomersList:
+  fileContents = f'{fileHeader}\nTime: {timeString}\n'
+  for customerId in customerList:
     fileContents += f'{customerId}\n'
-  
+    
   # Push to S3.
-  client.put_object(Body=fileContents, Bucket='siua-bucket', Key='bill_these_customers.txt')
-  print(fileContents)
+  retval = client.put_object(Body = fileContents, Bucket = bucketName, Key = keyPath) 
+  return retval
+
+def main():
+  
+  # Our customer IDs.
+  billableCustomersList = ['123', '456', '789'] # Square brackets indicate a list.
+
+  # Store a file from our customer list into S3.
+  createFileS3(customerList = billableCustomersList, fileHeader = '*** Marty was here ***', bucketName = 'siua-bucket', keyPath = 'demo/bill_these_customers.txt' )
+
 
 if __name__ == "__main__":  
   main()
