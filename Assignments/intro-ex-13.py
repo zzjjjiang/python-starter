39f9ded0efcdcf7a5f086d653854d1c293b56816
Martin Burolla <mburolla@xpanxion.com>
2021-10-27 09:54:46 -0400

    update

--- Assignments/intro-ex-13.py
+++ Assignments/intro-ex-13.py
@@ -5,19 +5,22 @@
 # Desc: Add Only Calculator
 #
 
-def shouldExit(inStr):
-  return inStr == "exit"
+def should_exit(instr):
+    return instr == "exit"
+
 
 def main():
-  print("Type exit at anytime to end program...")
-  while (True):
-    operand1 = input("Enter first integer: ")
-    if shouldExit(operand1):
-      break
-    operand2 = input("Enter second integer: ")
-    if shouldExit(operand2):
-      break
-    print(f'Answer: {int(operand1) + int(operand2)}.')
-  
+    print("Type exit at anytime to end program...")
+    while True:
+        operand1 = input("Enter first integer: ")
+        if should_exit(operand1):
+            break
+        operand2 = input("Enter second integer: ")
+        if should_exit(operand2):
+            break
+        total = int(operand1) + int(operand2);
+        print(f'Answer: {total}.')
+
+
 if __name__ == "__main__":  
-  main()
+    main()
